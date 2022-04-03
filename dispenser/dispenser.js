d6fb55fc06e7c9890f8209055fcd6a33f51b9448
caty-cristy <sambrisecaterina@gmail.com>
2022-04-03 16:12:16 +0300

    Add styles to our Joker

---
+++ dispenser/dispenser.js
@@ -0,0 +1,9 @@
+const giveMeAJoke = require("give-me-a-joke");
+const colors = require("colors");
+
+//Get a random dad Joke
+
+giveMeAJoke.getRandomDadJoke(function(joke) {
+    console.log(joke.white.underline);
+});
+
